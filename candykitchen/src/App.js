e8c78b74a055692d3204295b8ae0827300699be1
leul mekonnen <leulmekonnen049@gmail.com>
2023-11-06 06:10:16 -0800

    candy kitchen react app

---
+++ candykitchen/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Components/Layout/Header';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+     <h1>My App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ candykitchen/src/Components/Layout/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <React.Fragment>
+      <div className="header">
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+        </div>
+        <div className="main-image">
+         
+        </div>
+      
+    </React.Fragment>
+  );
+};
+
+export default Header;
+
